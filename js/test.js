b15d2d1c625e82b5fc47a63ecb71c401061e8a15
noahjahn <36347998+noahjahn@users.noreply.github.com>
2018-10-07 16:02:51 -0400

    Tested js code with gallery, learned that we need to use server side js if we want to read the file system on the server.

--- js/test.js
+++ js/test.js
@@ -1,12 +1,18 @@
-/*var scripts= document.getElementsByTagName('script');
+var scripts= document.getElementsByTagName('script');
 var path= scripts[scripts.length-1].src.split('?')[0];      // remove any ?query
-var mydir= path.split('/').slice(0, -1).join('/')+'/';  // remove last filename part of path*/
-var fs = require('fs');
-var files = fs.readdirSync("C:/Users/Noah/Google Drive/Files/School/WMU/2018-3 Fall/CS3500/Project/herecomestreblewmu/images/");
+var mydir= path.split('/').slice(0, -1).join('/')+'/';  // remove last filename part of path
+
+console.log(mydir);
+
+var files = readdirSync("mydir/../images/");
+
+var colClass = "col-md-2"
 
 var i = 0;
-
 while (i < files.length) {
+  document.write("<div class='" + colClass  + "' >");
+  document.write("<img src=images/" + files[i]);
+  document.write("</div>");
   console.log(i + " " + files[i]);
   i++;
 }

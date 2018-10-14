b722ee275b943f32d7b1edda4a524d9cedcbe179
Noah Jahn <najahn19@gmail.com>
2018-10-14 10:17:00 -0400

    Finished Gallery with new javascript.

--- js/imageSlideShow.js
+++ js/imageSlideShow.js
@@ -1,4 +1,4 @@
-/*function openModal() {
+function openModal() {
   document.getElementById('myModal').style.display = "block";
 }
 
@@ -34,4 +34,4 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
-}*/
+}
--- js/loadImages.js
+++ js/loadImages.js
@@ -1,9 +1,33 @@
 function loadImages() {
-  var images = ["img (189).jpg", "img (190).jpg", "img (191).jpg", "img (192).jpg", "img (193).jpg", "img (195).jpg", "img (196).jpg", "img (197).jpg"]
+  var images = ["img (189).jpg", "img (190).jpg", "img (191).jpg", "img (192).jpg", "img (193).jpg", "img (195).jpg", "img (196).jpg", "img (197).jpg"];
 
-  for (i = 0; i < images.length; i++){
-
-      document.write(("<div class=\"col\"><img src=\"images/") + images[i] + "\"style=\"width:100%\" onclick=\"openModal();currentSlide(1)\" class=\"hover-shadow cursor\"></div>");
-      console.log(images[i]);
+  for (i = 0; i < images.length; i++) {
+      if (i % 4 == 0) {
+        document.write("</div>");
+        document.write("<div class=\"row\">");
+      }
+      document.write(("<div class=\"col-md\"><img src=\"images/") + images[i] + ("\"style=\"width:99%; margin-bottom: 1em;\" onclick=\"openModal();currentSlide(") + (i+1) + (")\" class=\"hover-shadow\"></div>"));
+      //console.log(images[i]);
   }
+}
+
+function loadModal() {
+    var images = ["img (189).jpg", "img (190).jpg", "img (191).jpg", "img (192).jpg", "img (193).jpg", "img (195).jpg", "img (196).jpg", "img (197).jpg"];
+    var total = images.length;
+
+    for (i = 0; i < total; i++) {
+        document.write("<div class=\"mySlides\">");
+        document.write(("<div class=\"numbertext\">") + (i+1) + (" / ") + total + ("</div>"));
+        document.write(("<img src=\"images/") + images[i] + ("\" style=\"width: 100%;\">"));
+        document.write("</div>");
+    }
+
+    document.write("<a class=\"prev\" onclick=\"plusSlides(-1)\">&#10094;</a>");
+    document.write("<a class=\"next\" onclick=\"plusSlides(1)\">&#10095;</a>");
+
+    // for (i = 0; i < total; i++) {
+    //     document.write("<div class=\"column\">");
+    //     document.write(("<img class=\"demo cursor\" src=\"images/") + images[i] + ("\" style=\"width: 100%;\" onclick=\"currentSlide(") + i + (")\""));
+    //     document.write("</div>");
+    // }
 }

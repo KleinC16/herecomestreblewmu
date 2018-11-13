d7784d9bd0b4b20403ec8ebcf84f1d90fc7bd540
noahjahn <36347998+noahjahn@users.noreply.github.com>
2018-11-13 11:47:38 -0500

    updated code

--- db/herecomestreblewmu.sql
+++ db/herecomestreblewmu.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.2
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 13, 2018 at 03:13 AM
--- Server version: 10.1.34-MariaDB
--- PHP Version: 7.2.7
+-- Generation Time: Nov 13, 2018 at 05:44 PM
+-- Server version: 10.1.36-MariaDB
+-- PHP Version: 7.2.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,19 +25,58 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `bios`
+--
+
+CREATE TABLE `bios` (
+  `biosid` int(11) NOT NULL,
+  `username` varchar(32) NOT NULL,
+  `position` varchar(16) DEFAULT NULL,
+  `classification` varchar(10) NOT NULL,
+  `major` varchar(32) NOT NULL,
+  `description` varchar(512) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `bios`
+--
+
+INSERT INTO `bios` (`biosid`, `username`, `position`, `classification`, `major`, `description`) VALUES
+(1, 'briana.klug', 'Secretary', 'Soprano', 'Elementary Eductaion', 'I\'m from Muskegon, Michigan and currently in my fourth year at WMU. I love: singing, teaching, learning, and traveling. I\'ve enjoyed being a part of such a beautiful a capella group since its foundation and watching our continual growth.');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
+  `firstname` varchar(32) NOT NULL,
+  `lastname` varchar(32) NOT NULL,
   `email` varchar(64) NOT NULL,
   `username` varchar(32) NOT NULL,
-  `password` binary(64) NOT NULL
+  `password` varchar(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `username`, `password`) VALUES
+(1, 'Briana', 'Klug', 'briana.klug@gmail.com', 'briana.klug', '123456');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `bios`
+--
+ALTER TABLE `bios`
+  ADD PRIMARY KEY (`biosid`),
+  ADD UNIQUE KEY `biosid` (`biosid`),
+  ADD UNIQUE KEY `biosid_2` (`biosid`);
 
 --
 -- Indexes for table `users`
@@ -45,6 +84,22 @@ CREATE TABLE `users` (
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `id` (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bios`
+--
+ALTER TABLE `bios`
+  MODIFY `biosid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

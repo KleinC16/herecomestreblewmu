7775227204ae13541fcd1bd16de45fb270b18875
noahjahn <36347998+noahjahn@users.noreply.github.com>
2018-11-24 01:13:44 -0500

    Changed the executive board row count

--- application/views/main.php
+++ application/views/main.php
@@ -29,19 +29,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <!-- - - - Start Bios - - - -->
         <a name="members"><h1 class="text-center">Meet the Members</h1></a>
         <?php
-            function display_executive($member, $count) {
+            function write_members($member) {
                 $fullname = $member->firstname . " " . $member->lastname;
-                if ($count != 5) {
-                    echo '<div class="col-md-3 d-flex align-items-stretch">';
-                } else {
-                    echo '<div class="col-md d-flex align-items-stretch">'; // try adjusting rows based on screen width... figure out what looks at each pixel size
-                }
-
+                echo '<div class="col-lg-3 d-flex align-items-stretch justify-content-md-center">';
                 echo '<div class="new_card">';
                 echo '<img src="assets/images/users/' . strtolower($member->firstname) . '.' . strtolower($member->lastname) . '.jpg" alt="' . $fullname . '" style="width:100%;">';
                 echo '<div class="container">';
                 echo '<h2>' . $fullname . '</h2>';
-                echo '<p class="title">' . $member->position . '</p>';
+                if ($member->position != NULL) {
+                    echo '<p class="title">' . $member->position . '</p>';
+                }
                 echo '<p class="title">' . $member->classification . '</p>';
                 echo '<p>' . $member->description . '</p>';
                 echo '</div>';
@@ -49,18 +46,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 echo '</div>';
             }
 
-            function display_member($member, $count) {
-                $fullname = $member->firstname . " " . $member->lastname;
-                echo '<div class="col-md-3 d-flex align-items-stretch justify-content-md-center">';
-                echo '<div class="new_card">';
-                echo '<img src="assets/images/users/' . strtolower($member->firstname) . '.' . strtolower($member->lastname) . '.jpg" alt="' . $fullname . '" style="width:100%;">';
-                echo '<div class="container">';
-                echo '<h2>' . $fullname . '</h2>';
-                echo '<p class="title">' . $member->classification . '</p>';
-                echo '<p>' . $member->description . '</p>';
-                echo '</div>';
-                echo '</div>';
-                echo '</div>';
+            function cmp ($o1, $o2) {
+                return strcmp($o1->firstname, $o2->$firstname);
             }
 
             $executiveq = $this->db->query("SELECT firstname, lastname, position, classification, major, description
@@ -76,11 +63,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             $i = 0;
             foreach($executiveq->result() as $row) {
                 if ($row->position != NULL) {
-                    display_executive($row, $numExecutives);
+                    write_members($row, $numExecutives);
                 }
                 $i++;
                 if ($numExecutives > 5) {
                     if ($i % 4 == 0) {
+                        echo '</div>';
+                        echo '<br>';
+                        echo '<div class="row justify-content-md-center">';
+                    }
+                } elseif ($numExecutives = 5) {
+                    if ($i % 3 == 0) {
                         echo '</div>';
                         echo '<br>';
                         echo '<div class="row justify-content-md-center">';
@@ -93,16 +86,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                                 FROM bios
                                                 JOIN users
                                                 WHERE bios.username = users.username
-                                                AND position IS NULL");
+                                                AND position IS NULL
+                                                ORDER BY firstname");
             $numMembers = $memberq->num_rows();
-            //sort($memberq);
 
             echo '<h2><em>Members</em></h2>';
             echo '<div class="row justify-content-md-center">';
             $i = 0;
             foreach($memberq->result() as $row) {
                 if ($row->position == NULL) {
-                    display_member($row, $numMembers);
+                    write_members($row, $numMembers);
                 }
                 $i++;
                 if ($i % 4 == 0) {

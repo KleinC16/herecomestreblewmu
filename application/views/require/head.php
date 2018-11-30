398793b69f05663eb033409a94e75d6546e7f9c1
Chandler <chandler.j.klein@wmich.edu>
2018-11-30 18:14:04 -0500

    added stuff to gallery upload and gallery default

--- application/views/require/head.php
+++ application/views/require/head.php
@@ -3,6 +3,10 @@
     <link rel="stylesheet" href="/<?php echo base_url();?>assets/css/bootstrap.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="/<?php echo base_url();?>assets/css/main.css" />
+    <link rel="stylesheet" href="/<?php echo base_url();?>assets/css/upload.css">
+    <link rel="stylesheet" href="/<?php echo base_url();?>assets/css/gallery.css" />
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://bootswatch.com/_vendor/bootstrap/dist/js/bootstrap.min.js"></script>
+    
 </head>

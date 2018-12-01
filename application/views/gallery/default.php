d2de0922955fc12181b4484a982362db7a957a38
noahjahn <36347998+noahjahn@users.noreply.github.com>
2018-11-30 19:19:59 -0500

    Fixed the links I broke

--- application/views/gallery/default.php
+++ application/views/gallery/default.php
@@ -11,12 +11,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<div class="card-deck">
 			<div class="Instagram-card">
 				<div class="Instagram-card-header">
-					<img src="/<?php echo base_url();?>assets/images/index.png"class="Instagram-card-user-image">
+					<img src="<?php echo base_url();?>assets/images/index.png"class="Instagram-card-user-image">
 					<a class="Instagram-card-user-name" href=""> username </a>
 					<div class="Instagram-card-time"> 1 hour ago </div>
 				</div>
 				<div class="Instagram-card-image">
-					<img src= "/<?php echo base_url();?>assets/images/bongo-cat.svg" height=600px/>
+					<img src= "<?php echo base_url();?>assets/images/bongo-cat.svg" height=600px/>
 				</div>
 				<div class="Instagram-card-content">
 					<p class="Likes">2,568,990 likes</p>
@@ -26,7 +26,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 						<p class="comments"> see 12 more comments </p>
 						<br><a class="user-comment" href="#">username2</a> Hello</br>
 						<hr>
-					</div>  
+					</div>
 					<div class="Instagram-card-footer">
 						<a class="footer-action-icons"href="#"><i class="fa fa-heart-o"></i></a>
 						<input class="comments-input" type="text" placeholder="Add a comment..."/>
--- application/views/require/head.php
+++ application/views/require/head.php
@@ -2,12 +2,12 @@
     <title>Here Comes Treble</title>
     <meta charset = "utf-8">
     <meta name="viewport" content="width=device-width">
-    
-    <link rel="stylesheet" href="/<?php echo base_url();?>assets/css/bootstrap.css" />
+
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="/<?php echo base_url();?>assets/css/main.css" />
-    <link rel="stylesheet" href="/<?php echo base_url();?>assets/css/upload.css">
-    <link rel="stylesheet" href="/<?php echo base_url();?>assets/css/gallery.css" />
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/main.css" />
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/upload.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/gallery.css" />
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://bootswatch.com/_vendor/bootstrap/dist/js/bootstrap.min.js"></script>

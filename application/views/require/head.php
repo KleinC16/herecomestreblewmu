cffcba58d595fe58ec5051b0b0c102cc35c4e633
Chandler <chandler.j.klein@wmich.edu>
2018-11-30 19:17:43 -0500

    gallery cards

--- application/views/require/head.php
+++ application/views/require/head.php
@@ -1,11 +1,15 @@
 <head>
     <title>Here Comes Treble</title>
-    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.css" />
+    <meta charset = "utf-8">
+    <meta name="viewport" content="width=device-width">
+    
+    <link rel="stylesheet" href="/<?php echo base_url();?>assets/css/bootstrap.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/main.css" />
-    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/upload.css" />
-    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/gallery.css" />
+    <link rel="stylesheet" href="/<?php echo base_url();?>assets/css/main.css" />
+    <link rel="stylesheet" href="/<?php echo base_url();?>assets/css/upload.css">
+    <link rel="stylesheet" href="/<?php echo base_url();?>assets/css/gallery.css" />
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://bootswatch.com/_vendor/bootstrap/dist/js/bootstrap.min.js"></script>
+
 </head>
-    

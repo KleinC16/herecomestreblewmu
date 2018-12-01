3983dbce17bad06d7bd76ed316eff2f489055ab6
noahjahn <36347998+noahjahn@users.noreply.github.com>
2018-12-01 11:45:15 -0500

    Added new database

--- db/herecomestreblewmu.sql
+++ db/herecomestreblewmu.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 01, 2018 at 01:43 AM
+-- Generation Time: Dec 01, 2018 at 05:42 PM
 -- Server version: 10.1.34-MariaDB
 -- PHP Version: 7.2.7
 
@@ -27,6 +27,7 @@ SET time_zone = "+00:00";
 --
 -- Table structure for table `bios`
 --
+
 DROP TABLE IF EXISTS `bios`;
 
 CREATE TABLE `bios` (
@@ -35,32 +36,33 @@ CREATE TABLE `bios` (
   `position` varchar(16) DEFAULT NULL,
   `classification` varchar(42) NOT NULL,
   `major` varchar(64) NOT NULL,
-  `description` varchar(576) NOT NULL
+  `description` varchar(576) NOT NULL,
+  `complete` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `bios`
 --
 
-INSERT INTO `bios` (`biosid`, `username`, `position`, `classification`, `major`, `description`) VALUES
-(1, 'briana.klug', 'Secretary', 'Soprano', 'Elementary Eductaion', 'I\'m from Muskegon, Michigan and currently in my fourth year at WMU. I love: singing, teaching, learning, and traveling. I\'ve enjoyed being a part of such a beautiful a capella group since its foundation and watching our continual growth.'),
-(2, 'christopher.frego', 'President', 'Alto', 'Engineering Design Technology', 'I have been singing/performing for over 7 years now, from show choir, to dance, to singing. I come from a long line of musicians and music lovers, so music has been a keystone in my life. I still have three years left of school, but I plan to stay in Here Comes Treble every step!'),
-(3, 'maddie.pierce', 'Vice President', 'Alto', 'Photography & Intermedia', 'I love TV shows, art, and creating art, music, and singing! I have a year and a half left of school and I\'m eager to see where life takes me. Here Comes Treble has been such a huge part of my college experience and I\'m so happy and proud to be a part of this wonderful creation of an a capella group. I truly love my HCT family!'),
-(4, 'anna.sitar', 'Treasurer', 'Alto', 'Mechanical Engineering', 'Hi friends! I am originally from Marshall, MI. I am an avid photographer and videographer and adore getting to know new people. I joined Here Comes Treble in 2016 at the start of the group and fell in love with not only its members but the way making music with these amazing humans felt. I can\'t wait to continue my a capella career this year!'),
-(5, 'emma.dunlop', 'Music Director', 'Alto', 'Acting Performance', 'I\'m a Junior at WMU. I\'m the token Canadian in the group! I love theatre, music, and anything to do with the arts. I\'m so lucky to be in such an amazing a capella group and make amazing music every week!'),
-(6, 'niang.siam', NULL, 'Alto', 'Elementary Education', 'I\'m a Sophomore at WMU. I\'m originally from Myanmar (Burma). I love music, singing, and artistic people. In my free time I watch movies, hang out with babies, or do community service. I feel blessed to be part of this group because it made me feel more confident about myself. I\'m glad I joined Here Comes Treble a capella group.'),
-(7, 'ryan.loney', NULL, 'Beatboxer, Backup Dancer, Group Hypeman', 'Sales and Business Marketing', 'I am from Kalamazoo, MI. My first love is sports, especially football and rugby, however I have discovered new passion since joining HCT as a Sophomore in the Winter of 2016. Outside of sports and HCT I love reading, watching movies, and giving all my love to my puppies.'),
-(8, 'kylee.wheeler', NULL, 'Alto', 'Secondary Education, English and Earth Science', 'I\'m a Freshman at WMU! I\'ve been singing as long as I can remember and I absolutely love being in Here Comes Treble! In the few weeks I\'ve been here, HCT has been such a kind, welcoming family and I wouldn\'t want to be anywhere else. When I\'m not singing, I love to read, listen to music, watch America\'s Next Top Model, and tend to my bees. I\'m so happy to be part of a community filled with so much support and positivity. I love being a part of this group!!'),
-(9, 'chelsea.chavez', NULL, 'Alto', 'Business and Communications', 'I love writing poetry, playing guitar, and crafting/upscaling clothing, jewelry, and furniture. I am 23 years old with an ever-growing passion for creating art including music. Here Comes Treble is my first experience in an a capella group and I am so thrilled to grow with everyone.'),
-(10, 'ophelia.williams', NULL, 'Alto', 'Pyschology', 'I\'m a freshamn at WMU. I love music with all my heart and am blessed to be in an amazing a capella group. I was born into a very musical family and have been singing since birth. When I\'m not singing I love to read, write, dance, and watch TV. I plan to stay in the group HCT for the rest of my college career because since I\'ve joined HCT I\'ve met so many amazing people and have made amazing memories that will last a lifetime. I love this group and am blessed to have the opportunity to sing with them!'),
-(11, 'elizabeth.taylor', NULL, 'Alto', 'Public Relations', 'I love to sing and am an excitable ball of energy. I aspire to be happy in whatever career I choose. In addition to this, I would hope to leave the world better than when I came in it. I love food! Here Comes Treble is awesome, so I look forward to great things with the group.'),
-(12, 'jillian.brown', NULL, 'Soprano', 'English Rhetoric and Writing Studies', 'I have been singing for the majority of my life and I cannot think of a better way to spend my free time! I love all things musical, as well as theatre, reading, animals, watching The Office, and spending time with my cats. Shout out to my AMAZING family for always supporting my crazy dreams and for not kicking me out of the house every time I refused to stop singing Les Mis music at the top of my lungs. I love love LOVE  my Treble family and I\'m so excited to make some SICK music with these peeps this year'),
-(13, 'cat.vaught', NULL, 'Soprano', 'Political Science', 'I am a Freshman here at Western. I\'m originally from Traverse City, Michigan. I love musical theatre and I am super passionate about politics. I\'m so excited to be a part of Here Comes Treble this year and I\'m so happy it comes with so many great connections and relationships as well!'),
-(14, 'kitty.clark', NULL, 'Soprano', 'Vocal Performance', 'I am a Freshman at WMU. I\'ve been performing since I can remember and I\'m so happy to be a part of HCT! When I\'m not signing, I love to read, do community service, and watch movies. I could not imagine a better group of people to perform with and I\'m so thankful to have met these amazing people!'),
-(15, 'kailyn.riebe', NULL, 'Soprano', 'Secondary Education', 'I am from Berkley, Michigan. I am a Sophomore at Western. I have done theatre since I was eight years old, dance since I was three ears old, and singing a good majority of my life. I love the performing arts in general, it is a passion of mine. I am really excited to be a part of Here Comes Treble, I feel like I am already a part of a family. They even tolerate my bad dance moves! I am so lucky to have the opportunity to make music alongside all of these amazingly talented people!'),
-(16, 'meggie.anderson', NULL, 'Soprano', 'Education, WMU Grad', 'I\'m a WMU graduate. I\'ve been a musical theatre kid since way back. While other kids in gym class were strategizing how to win the sports games, I was in the outfield choreographing dances and picking dandelions. In my freetime I like to read books about magic and mysteries. I love our a capella group because I think it\'s so important to have a good group of genuine, no-drama friends, and it\'s even cooler when we all have a love of singing in common!'),
-(17, 'haley.schnoor', NULL, 'Alto', 'Special Education', 'I\'m a super super Senior after chaning my major, but I\'ve finally found my niche. I love music, dramatic TV shows, reading, Taco Bell, and snuggling my cat. I am thrilled to be a part of HCT this year and I already love it!'),
-(18, 'noah.jahn', NULL, 'Tenor', 'Computer Science', 'I am in the computer science major and I don\'t sing in this group.');
+INSERT INTO `bios` (`biosid`, `username`, `position`, `classification`, `major`, `description`, `complete`) VALUES
+(1, 'briana.klug', 'Secretary', 'Soprano', 'Elementary Eductaion', 'I\'m from Muskegon, Michigan and currently in my fourth year at WMU. I love: singing, teaching, learning, and traveling. I\'ve enjoyed being a part of such a beautiful a capella group since its foundation and watching our continual growth.', 1),
+(2, 'christopher.frego', 'President', 'Alto', 'Engineering Design Technology', 'I have been singing/performing for over 7 years now, from show choir, to dance, to singing. I come from a long line of musicians and music lovers, so music has been a keystone in my life. I still have three years left of school, but I plan to stay in Here Comes Treble every step!', 0),
+(3, 'maddie.pierce', 'Vice President', 'Alto', 'Photography & Intermedia', 'I love TV shows, art, and creating art, music, and singing! I have a year and a half left of school and I\'m eager to see where life takes me. Here Comes Treble has been such a huge part of my college experience and I\'m so happy and proud to be a part of this wonderful creation of an a capella group. I truly love my HCT family!', 0),
+(4, 'anna.sitar', 'Treasurer', 'Alto', 'Mechanical Engineering', 'Hi friends! I am originally from Marshall, MI. I am an avid photographer and videographer and adore getting to know new people. I joined Here Comes Treble in 2016 at the start of the group and fell in love with not only its members but the way making music with these amazing humans felt. I can\'t wait to continue my a capella career this year!', 0),
+(5, 'emma.dunlop', 'Music Director', 'Alto', 'Acting Performance', 'I\'m a Junior at WMU. I\'m the token Canadian in the group! I love theatre, music, and anything to do with the arts. I\'m so lucky to be in such an amazing a capella group and make amazing music every week!', 0),
+(6, 'niang.siam', NULL, 'Alto', 'Elementary Education', 'I\'m a Sophomore at WMU. I\'m originally from Myanmar (Burma). I love music, singing, and artistic people. In my free time I watch movies, hang out with babies, or do community service. I feel blessed to be part of this group because it made me feel more confident about myself. I\'m glad I joined Here Comes Treble a capella group.', 0),
+(7, 'ryan.loney', NULL, 'Beatboxer, Backup Dancer, Group Hypeman', 'Sales and Business Marketing', 'I am from Kalamazoo, MI. My first love is sports, especially football and rugby, however I have discovered new passion since joining HCT as a Sophomore in the Winter of 2016. Outside of sports and HCT I love reading, watching movies, and giving all my love to my puppies.', 0),
+(8, 'kylee.wheeler', NULL, 'Alto', 'Secondary Education, English and Earth Science', 'I\'m a Freshman at WMU! I\'ve been singing as long as I can remember and I absolutely love being in Here Comes Treble! In the few weeks I\'ve been here, HCT has been such a kind, welcoming family and I wouldn\'t want to be anywhere else. When I\'m not singing, I love to read, listen to music, watch America\'s Next Top Model, and tend to my bees. I\'m so happy to be part of a community filled with so much support and positivity. I love being a part of this group!!', 0),
+(9, 'chelsea.chavez', NULL, 'Alto', 'Business and Communications', 'I love writing poetry, playing guitar, and crafting/upscaling clothing, jewelry, and furniture. I am 23 years old with an ever-growing passion for creating art including music. Here Comes Treble is my first experience in an a capella group and I am so thrilled to grow with everyone.', 0),
+(10, 'ophelia.williams', NULL, 'Alto', 'Pyschology', 'I\'m a freshamn at WMU. I love music with all my heart and am blessed to be in an amazing a capella group. I was born into a very musical family and have been singing since birth. When I\'m not singing I love to read, write, dance, and watch TV. I plan to stay in the group HCT for the rest of my college career because since I\'ve joined HCT I\'ve met so many amazing people and have made amazing memories that will last a lifetime. I love this group and am blessed to have the opportunity to sing with them!', 0),
+(11, 'elizabeth.taylor', NULL, 'Alto', 'Public Relations', 'I love to sing and am an excitable ball of energy. I aspire to be happy in whatever career I choose. In addition to this, I would hope to leave the world better than when I came in it. I love food! Here Comes Treble is awesome, so I look forward to great things with the group.', 0),
+(12, 'jillian.brown', NULL, 'Soprano', 'English Rhetoric and Writing Studies', 'I have been singing for the majority of my life and I cannot think of a better way to spend my free time! I love all things musical, as well as theatre, reading, animals, watching The Office, and spending time with my cats. Shout out to my AMAZING family for always supporting my crazy dreams and for not kicking me out of the house every time I refused to stop singing Les Mis music at the top of my lungs. I love love LOVE  my Treble family and I\'m so excited to make some SICK music with these peeps this year', 0),
+(13, 'cat.vaught', NULL, 'Soprano', 'Political Science', 'I am a Freshman here at Western. I\'m originally from Traverse City, Michigan. I love musical theatre and I am super passionate about politics. I\'m so excited to be a part of Here Comes Treble this year and I\'m so happy it comes with so many great connections and relationships as well!', 0),
+(14, 'kitty.clark', NULL, 'Soprano', 'Vocal Performance', 'I am a Freshman at WMU. I\'ve been performing since I can remember and I\'m so happy to be a part of HCT! When I\'m not signing, I love to read, do community service, and watch movies. I could not imagine a better group of people to perform with and I\'m so thankful to have met these amazing people!', 0),
+(15, 'kailyn.riebe', NULL, 'Soprano', 'Secondary Education', 'I am from Berkley, Michigan. I am a Sophomore at Western. I have done theatre since I was eight years old, dance since I was three ears old, and singing a good majority of my life. I love the performing arts in general, it is a passion of mine. I am really excited to be a part of Here Comes Treble, I feel like I am already a part of a family. They even tolerate my bad dance moves! I am so lucky to have the opportunity to make music alongside all of these amazingly talented people!', 0),
+(16, 'meggie.anderson', NULL, 'Soprano', 'Education, WMU Grad', 'I\'m a WMU graduate. I\'ve been a musical theatre kid since way back. While other kids in gym class were strategizing how to win the sports games, I was in the outfield choreographing dances and picking dandelions. In my freetime I like to read books about magic and mysteries. I love our a capella group because I think it\'s so important to have a good group of genuine, no-drama friends, and it\'s even cooler when we all have a love of singing in common!', 0),
+(17, 'haley.schnoor', NULL, 'Alto', 'Special Education', 'I\'m a super super Senior after chaning my major, but I\'ve finally found my niche. I love music, dramatic TV shows, reading, Taco Bell, and snuggling my cat. I am thrilled to be a part of HCT this year and I already love it!', 0),
+(18, 'noah.jahn', NULL, 'Tenor', 'Computer Science', 'I am in the computer science major and I don\'t sing in this group.', 0);
 
 -- --------------------------------------------------------
 
@@ -140,7 +142,8 @@ INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `username`, `passwo
 (14, 'Kitty', 'Clark', '', 'kitty.clark', '123456'),
 (15, 'Kailyn', 'Riebe', '', 'kailyn.riebe', '123456'),
 (16, 'Meggie', 'Anderson', '', 'meggie.anderson', '123456'),
-(17, 'Haley', 'Schnoor', '', 'haley.schnoor', '123456');
+(17, 'Haley', 'Schnoor', '', 'haley.schnoor', '123456'),
+(21, 'Noah', 'Jahn', 'noah.a.jahn@wmich.edu', 'noah.jahn', '123456');
 
 --
 -- Indexes for dumped tables
@@ -188,7 +191,7 @@ ALTER TABLE `gallery`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

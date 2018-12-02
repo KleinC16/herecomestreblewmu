003bd5848d8e677eba2c019e907609a8dbc45672
Chandler <chandler.j.klein@wmich.edu>
2018-12-02 15:13:40 -0500

    completed upload

--- application/views/gallery/upload.php
+++ application/views/gallery/upload.php
@@ -11,10 +11,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 		<form action ="upload.php" method="post" enctype="multipart/form-data">
 			Select an image to upload:
-			<input type="file" name="fileToUpload" id="fileToUpload">
-			<input type="submit" value="Upload Image" name="submit">
-
+			<input type="file" name="fileToUpload" id="fileToUpload"><br><br>
+			Give it a title:<br>
+			<input type="text" name="title"><br><br>
+			Give it a description:<br>
+			<input type="text" name="description"><br><br>
+			Add tags:<br>
+			<input type="text" name="tags"><br><br>
+			<input type="submit" value="Upload" name="submit">
 		</form>
+		
 	</div>
 	<?php $this->load->view('require/footer.php'); ?>
 </body>

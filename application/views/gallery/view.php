c776ee291fc1bd26c9b5684eaa167e3c6cf450ac
Chandler <chandler.j.klein@wmich.edu>
2018-12-02 17:35:09 -0500

    deleted old gallery view code

--- application/views/gallery/view.php
+++ application/views/gallery/view.php
@@ -39,38 +39,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		</div>
 
 	</div>
-<!-- 		<div class="card-deck">
-			<div class="Instagram-card">
-				<div class="Instagram-card-header">
-					<img src="<?php echo base_url();?>assets/images/index.png"class="Instagram-card-user-image">
-					<a class="Instagram-card-user-name" href=""> username </a>
-					<div class="Instagram-card-time"> 1 hour ago </div>
-				</div>
-				<div class="Instagram-card-image">
-					<img class="img-responsive" src= "<?php echo base_url();?>assets/images/bongo-cat.svg" height=600px/>
-				</div>
-				
-				<div class="Instagram-card-content">
-					<p class="Likes">2,568,990 likes</p>
-					<p><a class="Instagram-card-content-user">
-						<href="#">username</a>
-						caption <a class="hashtag" href="#">#This is a hashtag</a></p>
-						<p class="comments"> see 12 more comments </p>
-						<br><a class="user-comment" href="#">username2</a> Hello</br>
-						<hr>
-					</div>
-					<div class="Instagram-card-footer">
-						<div class="likes-dislikes">
-							<a class="fa fa-arrow-up" href="#"></i></a>
-							<a class="fa fa-arrow-down" href="#"></i></a>
-						</div>
-						<a class="footer-action-icons"href="#"><i class="fa fa-heart-o"></i></a>
-						<input class="comments-input" type="text" placeholder="Add a comment..."/>
-						<a class="footer-action-icons"href="#"><i class="fa fa-ellipsis-h"></i></a>
-					</div>
-				</div>
-
-			-->
-			<?php $this->load->view('require/footer.php'); ?>
-		</body>
-		</html>
+	
+	<?php $this->load->view('require/footer.php'); ?>
+</body>
+</html>
